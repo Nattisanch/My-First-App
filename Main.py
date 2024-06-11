77c05d24eea9f6ab06b575733490500b107c27a3
Natalie <C916726@exelonds.com>
2024-06-11 14:18:57 -0400

    Added Log in

--- Main.py
+++ Main.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 from tkinter import *
-import customtkinter
+import customtkinter as ctk
 
 
 #setting up app and window/title etc
-customtkinter.set_appearance_mode("dark")
-root =customtkinter.CTk()
+ctk.set_appearance_mode("dark")
+root = ctk.CTk()
 root.title("Natalie's App")
 root.geometry ("600x350")
 
 #displays whats gonna be in app
-mybutton = customtkinter.CTkButton(root, text="Hey Natalie", corner_radius=40,fg_color="Pink",text_color="Black")
-mybutton.pack(pady=100)
 
+email=ctk.CTkEntry(root)
+email.pack(pady=10)
+
+passw=ctk.CTkEntry(root)
+passw.pack(pady=10)
+mybutton = ctk.CTkButton(root, text="Log In", corner_radius=40,fg_color="Pink",text_color="Black")
+mybutton.pack(pady=10)
+    
 root.mainloop()
-root.destroy()
