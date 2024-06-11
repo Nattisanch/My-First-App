21a264985d238bf800a35b316527809592b17370
Natalie <C916726@exelonds.com>
2024-06-11 14:39:52 -0400

    Fixed

--- Main.py
+++ Main.py
@@ -2,21 +2,23 @@ import tkinter as tk
 from tkinter import *
 import customtkinter as ctk
 
+def main():
+    ctk.set_appearance_mode("dark")
+    root = ctk.CTk()
+    root.title("Natalie's App")
+    root.geometry ("600x350")
 
-#setting up app and window/title etc
-ctk.set_appearance_mode("dark")
-root = ctk.CTk()
-root.title("Natalie's App")
-root.geometry ("600x350")
+    #displays whats gonna be in app
 
-#displays whats gonna be in app
+    email=ctk.CTkEntry(root)
+    email.pack(pady=10)
 
-email=ctk.CTkEntry(root)
-email.pack(pady=10)
+    passw=ctk.CTkEntry(root)
+    passw.pack(pady=10)
+    mybutton = ctk.CTkButton(root, text="Log In", corner_radius=40,fg_color="Pink",text_color="Black")
+    mybutton.pack(pady=10)
 
-passw=ctk.CTkEntry(root)
-passw.pack(pady=10)
-mybutton = ctk.CTkButton(root, text="Log In", corner_radius=40,fg_color="Pink",text_color="Black")
-mybutton.pack(pady=10)
-    
-root.mainloop()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
